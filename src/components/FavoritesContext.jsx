e4a068c8c5e1316c0f81272968d31db325a48b43
Tshireletso <tmogwere09@gmail.com>
2024-06-19 14:17:20 +0200

    declares favorite context state

---
+++ src/components/FavoritesContext.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const FavoritesContext = createContext();
+
+export const FavoritesProvider = ({ children }) => {
+    const [favorites, setFavorites] = useState([]);
+
+
+
+
+    
+}
