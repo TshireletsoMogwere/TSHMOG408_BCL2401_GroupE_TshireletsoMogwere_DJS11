dfe203f85b0a96efc2f4227707720e24fadf4565
Tshireletso <tmogwere09@gmail.com>
2024-06-19 14:19:22 +0200

    loads from local storage on initial render

--- src/components/FavoritesContext.jsx
+++ src/components/FavoritesContext.jsx
@@ -5,8 +5,13 @@ const FavoritesContext = createContext();
 export const FavoritesProvider = ({ children }) => {
     const [favorites, setFavorites] = useState([]);
 
+    useEffect(() => {
+        const storedFavorites = localStorage.getItem('favorites');
+        if (storedFavorites) {
+          setFavorites(JSON.parse(storedFavorites));
+        }
+      }, []);
 
 
 
-    
 }
